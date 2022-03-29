60d2e81d28f41820716a45b9ffc7ec77a873c8f4
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2022-03-29 11:15:18 +0300

    homework

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-console
+import Widget from './Widget';
+
+const app = new Widget();
+
+let text = 'Deserunt laborum ea ad incididunt nostrud veniam id proident elit adipisicing voluptate cillum proident. In elit consectetur veniam in laborum et tempor ut. Nisi sunt aute officia mollit veniam duis consectetur Lorem. Tempor nisi dolor irure veniam';
+text += 'Anim deserunt officia non ex do reprehenderit commodo sunt occaecat pariatur voluptate sit deserunt. Lorem occaecat ut non culpa deserunt laborum eiusmod esse aliquip irure non ipsum qui. Occaecat et do eu sint deserunt non anim do irure irure occaecat Lorem. Et qui pariatur do ullamco et ex ex eu sit non qui. Amet veniam ea ullamco eiusmod do enim incididunt cupidatat ea consequat.';
+
+app.renderWidget(text);
